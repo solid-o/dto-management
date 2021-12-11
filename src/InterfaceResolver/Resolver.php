708c735aa93c32a84f886e81e28255822d913e83
Alessandro Chitolina <alekitto@gmail.com>
2021-12-11 14:08:06 +0100

    throw exception if version cannot be derived from argument in resolver

--- src/InterfaceResolver/Resolver.php
+++ src/InterfaceResolver/Resolver.php
@@ -5,8 +5,14 @@ declare(strict_types=1);
 namespace Solido\DtoManagement\InterfaceResolver;
 
 use Psr\Http\Message\ServerRequestInterface;
+use Solido\DtoManagement\Exception\InvalidArgumentException;
 use Solido\DtoManagement\Finder\ServiceLocatorRegistryInterface;
+use Stringable;
 use Symfony\Component\HttpFoundation\Request;
+
+use function get_debug_type;
+use function is_string;
+use function Safe\sprintf;
 
 class Resolver implements ResolverInterface
 {
@@ -26,6 +32,14 @@ class Resolver implements ResolverInterface
             $version = $version->attributes->get('_version', 'latest');
         } elseif ($version instanceof ServerRequestInterface) {
             $version = $version->getAttribute('_version', 'latest');
+        }
+
+        if ($version !== null && ! is_string($version)) {
+            if (! $version instanceof Stringable) {
+                throw new InvalidArgumentException(sprintf('Version must be a string or a stringable object, %s passed', get_debug_type($version)));
+            }
+
+            $version = (string) $version;
         }
 
         return $this->registry->get($interface)->get($version ?? 'latest');
